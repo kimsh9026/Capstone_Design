63b1c27a5b563fac4a53da23db4d3843f3b1a847
beautylight <rwg3597@naver.com>
2019-04-10 16:09:31 +0900

    add logInPage

--- capstone/lib/AuthBloc.dart
+++ capstone/lib/AuthBloc.dart
@@ -3,5 +3,9 @@ import 'dart:async';
 
 
 class AuthBloc extends Object {
+  final _logIn = StreamController<bool>.broadcast() ;
 
+  Stream<bool> get logIn => _logIn.stream ;
+
+  Function(bool) get setlogIn => _logIn.sink.add ;
 }
