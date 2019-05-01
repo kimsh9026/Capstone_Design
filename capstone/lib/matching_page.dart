274901e21abb3b164594470f7404f2e5838cd8e9
keehyuckkk <dlrlgurrns@gmail.com>
2019-05-01 13:18:03 +0900

    map changed

--- capstone/lib/matching_page.dart
+++ capstone/lib/matching_page.dart
@@ -3,61 +3,69 @@ import 'package:capstone/fire_base_codes/fire_auth_provider.dart';
 import 'package:flutter/material.dart';
 //import 'package:capstone/test/chat_room_info.dart' ;
 import 'package:capstone/bottom_navigation.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 
 class MatchingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     print("MatchingPage Build") ;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Matching Page'),
-        actions:
-        <Widget>[
-          PopupMenuButton<BottomNavigationBarType>(
-            itemBuilder: (BuildContext context) => <PopupMenuItem<BottomNavigationBarType>>[
-              const PopupMenuItem<BottomNavigationBarType>(
-                value: BottomNavigationBarType.fixed,
-                child: Text('Something1'),
-              ),
-              const PopupMenuItem<BottomNavigationBarType>(
-                value: BottomNavigationBarType.shifting,
-                child: Text('something2'),
-              )
-            ],
-          )
-        ],
+    return MapApi();
+  }
+}
 
-      ),
-      body: Center(
-        child : Container(
-          color: Colors.blue,
-          width: double.infinity,
-          height: double.infinity,
-          child: Center(child: Column(
-            children: <Widget>[
-              Text(
-                'Matching..',
-                style: TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.white,
-                  decorationStyle: TextDecorationStyle.solid,
-                ),
-              ),
-              RaisedButton(
-                  child: Text('Sign out with Google'),
-                  onPressed: (){
-                    FireAuthProvider().signOut() ;
-                  }
-              )
-            ],
-          )),
+class MapApi extends StatefulWidget {
+  @override
+  State<MapApi> createState() => MapApiState();
+}
+
+class MapApiState extends State<MapApi> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  LatLng _lastMapPosition = _center;
+
+  final Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Trabuddying'),
+          backgroundColor: Colors.white,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: _markers,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
         ),
       ),
-//      bottomNavigationBar: botNavBar,
     );
-
-
-
   }
 }
